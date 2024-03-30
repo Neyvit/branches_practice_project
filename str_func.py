b4e1ba0dcc45619b2c12a16bdd4dd0e3d083c8b9
Neyvit <successfullproject@gmail.com>
2024-03-31 02:47:10 +0300

    added make_first_letter_capital function in str_func.py and docstring to its functions.

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
+"""
+This function makes every letter in a sentence CAPITAL
+"""
+
+
 def capitalize(string_value):
     return str(string_value).upper()
+
+
+"""
+This function makes every word start with a Capital Letter
+"""
+
+
+def make_first_letter_capital(string_value):
+    return str(string_value).title()
